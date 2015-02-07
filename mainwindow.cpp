87699b8d31fc4d617ee019e598891298f7a8f5bd
leeseemann <ls0013@uah.edu>
2015-02-07 10:39:21 -0600

    Added the ability to adapt to custom layouts
    Added the ability to adapt to custom layouts.  Also, created QList
    variables for the track segments, switches, and locomotives that can be
    accessed from other classes if needed.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "trainDisplayInfo.h"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,8 +10,16 @@ MainWindow::MainWindow(QWidget *parent) :
 
     ui->setupUi(this);
 
+    createTopLevelItems();
 
-    //TEST DATA
+    QVector<int> ids;
+    ids.push_back(33);
+    ids.push_back(34);
+    ids.push_back(35);
+
+    customLayout(ids, ids, ids);
+
+   /* //TEST DATA
     TrackSegments *seg1 = new TrackSegments();
     seg1->setTrackSegmentNumber("Segment 1");
     seg1->setComponentID(35);
@@ -67,8 +76,91 @@ MainWindow::MainWindow(QWidget *parent) :
     locomotive->addChild(loco1);
       locomotive->addChild(loco2);
 
-    ui->treeWidget->insertTopLevelItem(2,locomotive );
+    ui->treeWidget->insertTopLevelItem(2,locomotive );*/
 
+   // tracksegment->addChild(track.tracks.at(0));
+
+}
+
+
+// this function creates the three top level items in the TreeWidget
+void MainWindow::createTopLevelItems()
+{
+    tracksegment = new QTreeWidgetItem;
+    tracksegment->setText(0, "Track Segments");
+    ui->treeWidget->insertTopLevelItem(0, tracksegment);
+
+    trackswitch = new QTreeWidgetItem;
+    trackswitch->setText(0, "Track Switches");
+    ui->treeWidget->insertTopLevelItem(1, trackswitch);
+
+    locomotive = new QTreeWidgetItem;
+    locomotive->setText(0,"Locomotives");
+    ui->treeWidget->insertTopLevelItem(2, locomotive);
+
+}
+
+// this function populates three QLists that are then used to populate the TreeWidget
+void MainWindow::customLayout(QVector<int>track_ids, QVector<int>switch_ids, QVector<int>locomotive_ids)
+{
+    for(int i = 0; i < track_ids.size(); i++)
+    {
+        new_track = new TrackSegments;
+        QString segment = "Segment ";
+        QString segment_number = QString::number(i+1);
+        segment.append(segment_number);
+        new_track->setTrackSegmentNumber(segment);
+        new_track->setComponentID(track_ids.at(i));
+        new_track->setStatus("Inactive");
+
+        tracks.insert(i,new_track);
+    }
+
+    for(int i = 0; i < switch_ids.size(); i++)
+    {
+        new_switch = new TrackSwitches;
+        QString switch_label = "Switch ";
+        QString switch_number = QString::number(i+1);
+        switch_label.append(switch_number);
+        new_switch->setTrackSwitchNumber(switch_label);
+        new_switch->setComponentID(switch_ids.at(i));
+        new_switch->setStatus("Bypass");
+
+        switches.insert(i,new_switch);
+    }
+
+    for(int i = 0; i < locomotive_ids.size(); i++)
+    {
+        new_locomotive = new Locomotives;
+        QString locomotive_label = "Locomotive ";
+        QString locomotive_number = QString::number(i+1);
+        locomotive_label.append(locomotive_number);
+        new_locomotive->setLocomotiveNumber(locomotive_label);
+        new_locomotive->setComponentID(locomotive_ids.at(i));
+        new_locomotive->setStatus("");
+
+        locomotives.insert(i,new_locomotive);
+    }
+    addChildren(tracks, switches, locomotives);
+}
+
+// this function adds all the necessary data to the TreeWidget
+void MainWindow::addChildren(QList<TrackSegments*> tracks, QList<TrackSwitches*> switches, QList<Locomotives*> locomotives)
+{
+    for(int i = 0; i < tracks.size(); i++)
+    {
+        tracksegment->addChild(tracks.at(i));
+    }
+
+    for(int j = 0; j < switches.size(); j++)
+    {
+        trackswitch->addChild(switches.at(j));
+    }
+
+    for(int k = 0; k < locomotives.size(); k++)
+    {
+        locomotive->addChild(locomotives.at(k));
+    }
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTreeWidgetItem>
+#include "trainDisplayInfo.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,24 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    QList<TrackSegments*> tracks;
+    QList<TrackSwitches*> switches;
+    QList<Locomotives*> locomotives;
+    void customLayout(QVector<int>, QVector<int>, QVector<int>);
+
 private:
     Ui::MainWindow *ui;
+
+    QTreeWidgetItem *tracksegment;
+    QTreeWidgetItem *trackswitch;
+    QTreeWidgetItem *locomotive;
+    TrackSegments* new_track;
+    TrackSwitches* new_switch;
+    Locomotives* new_locomotive;
+
+    void createTopLevelItems();
+    void addChildren(QList<TrackSegments*>, QList<TrackSwitches*>, QList<Locomotives*>);
+
 };
 
 #endif // MAINWINDOW_H
