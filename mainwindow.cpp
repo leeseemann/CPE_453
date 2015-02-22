f517471a2e90e4c0964d14b26b9dd929572f1ffd
Falco Girgis <falco.girgis@elysianshadows.com>
2015-02-21 22:12:51 -0600

    Deleted some comments. 	# with '#' will be ignored, and an empty message aborts the commit.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -30,72 +30,7 @@ MainWindow::MainWindow(QWidget *parent) :
     sql_information();
 
 
-  //TEST DATA
-   /* TrackSegments *seg1 = new TrackSegments();
 
-    //set graphicsViewRectItem and position
-    seg1->setPos(100,100);
-    seg1->setRect(5,5,100, 100);
-
-    seg1->setTrackSegmentNumber("Segment 1");
-    seg1->setComponentID(35);
-    seg1->setStatus("Active");
-
-    TrackSegments *seg2 = new TrackSegments();
-    seg2->setTrackSegmentNumber("Segment 2");
-    seg2->setComponentID(36);
-    seg2->setStatus("Lauren");
-
-    TrackSegments *seg3 = new TrackSegments();
-    seg3->setTrackSegmentNumber("Segment 3");
-    seg3->setComponentID(37);
-    seg3->setStatus("Inactive");
-
-    TrackSwitches *switch1 = new TrackSwitches();
-    switch1->setTrackSwitchNumber("Switch 1");
-    switch1->setComponentID(21);
-    switch1->setStatus("Bypass");
-
-    TrackSwitches *switch2 = new TrackSwitches();
-    switch2->setTrackSwitchNumber("Switch 2");
-    switch2->setComponentID(22);
-    switch2->setStatus("Dog");
-
-    Locomotives *loco1 = new Locomotives();
-    loco1->setLocomotiveNumber("Locomotive 1");
-    loco1->setComponentID(88);
-    loco1->setStatus("On Segment 33");
-
-    Locomotives *loco2 = new Locomotives();
-    loco2->setLocomotiveNumber("Locomotive 2");
-    loco2->setComponentID(89);
-    loco2->setStatus("On Segment 37");
-
-   QTreeWidgetItem *trackSegmentParentItem = new QTreeWidgetItem();
-   trackSegmentParentItem->setText(0, "Track Segments");
-   trackSegmentParentItem->addChild(seg1);
-     trackSegmentParentItem->addChild(seg2);
-       trackSegmentParentItem->addChild(seg3);
-
-   ui->treeWidget->insertTopLevelItem(0,trackSegmentParentItem);
-
-
-   QTreeWidgetItem *trackSwitchParentItem = new QTreeWidgetItem();
-   trackSwitchParentItem->setText(0, "Track Switches");
-   trackSwitchParentItem->addChild(switch1);
-    trackSwitchParentItem->addChild(switch2);
-
-   ui->treeWidget->insertTopLevelItem(1,trackSwitchParentItem);
-
-    QTreeWidgetItem *locomotiveParentItem = new QTreeWidgetItem();
-    locomotiveParentItem->setText(0,"Locomotives");
-    locomotiveParentItem->addChild(loco1);
-      locomotiveParentItem->addChild(loco2);
-
-    ui->treeWidget->insertTopLevelItem(2,locomotiveParentItem );
-
-   // trackSegmentParentItem->addChild(track.tracks.at(0));
-*/
 }
 
 
